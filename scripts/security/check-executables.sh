ad47ffe3e5578a495ba7a4427b1fd1cc4b9de316
Loris Accordino <loris.accordino@gmail.com>
2025-12-04 23:54:35 +0100

    Fixed security script

--- scripts/security/check-executables.sh
+++ scripts/security/check-executables.sh
@@ -4,6 +4,9 @@ echo "Checking unauthorized executables..."
 failed=false
 
 for file in $(find . -type f \( -iname "*.exe" -o -iname "*.bat" -o -iname "*.sh" \)); do
+  # Remove leading ./
+  file_clean="${file#./}"
+
   # Allow scripts only in scripts/ folder
   if [[ "$file" != scripts/* ]]; then
     echo "Unauthorized executable found: $file"

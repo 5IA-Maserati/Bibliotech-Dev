ccba867153b21666994619b9b98b72f2831b90cf
Erbeso777 <165906048+ErBeso7@users.noreply.github.com>
2026-01-21 09:45:37 +0100

    Implemented the CSP inside the index.html

--- src/php/bootstrap.php
+++ src/php/bootstrap.php
@@ -5,3 +5,13 @@ header("X-Content-Type-Options: nosniff");
 header("X-Frame-Options: DENY");
 header("Referrer-Policy: strict-origin-when-cross-origin");
 ?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca Scolastica</title>
+
+    <!-- CSS principale -->
+    <link rel="stylesheet" href="/assets/style/index.css">
+</head>
--- src/php/index.php
+++ src/php/index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biblioteca Scolastica</title>
-    <!-- Stylesheet: main page styles -->
-    <link rel="stylesheet" href="assets/style/index.css">
-</head>
-
+<?php require_once __DIR__  . "/bootstrap.php"; ?>
 
 <body>
 

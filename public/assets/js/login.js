e097d25aad2b7a9857439c6cfce9299ae67903c3
AleNappi <nappi.alessandro@st.istitutomaserati.edu.it>
2026-01-23 13:02:26 +0100

    Linting solving

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,16 +1,16 @@
-document.getElementById("login-form").addEventListener("submit", function (e) {
-    e.preventDefault();
+document.getElementById('login-form').addEventListener('submit', function (e) {
+  e.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
 
-    if (username === "" || password === "") {
-        alert("Compila tutti i campi");
-        return;
-    }
+  if (username === '' || password === '') {
+    alert('Compila tutti i campi');
+    return;
+  }
 
-    // Simulazione login
-    alert("Login effettuato con successo!");
+  // Simulazione login
+  alert('Login effettuato con successo!');
     
-    // Qui in futuro potrai collegare un backend
+  // Qui in futuro potrai collegare un backend
 });

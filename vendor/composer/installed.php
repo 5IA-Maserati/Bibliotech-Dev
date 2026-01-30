3e5a1dd785f4db687c9fcad8d15c4716a00f3ceb
Erbe777 <opmarco7@gmail.com>
2026-01-30 19:59:47 +0100

    Change propieties inside the yml super-lintere

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a53c967162483bd993843e8199045f7d8d8023e0',
+        'reference' => 'cd0374e2514f44723d1c003a254ed19055953908',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a53c967162483bd993843e8199045f7d8d8023e0',
+            'reference' => 'cd0374e2514f44723d1c003a254ed19055953908',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),

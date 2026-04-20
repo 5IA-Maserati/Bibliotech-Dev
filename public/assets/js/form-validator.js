c2771a70ec92fa13df49fa6962aaa4fdb4d66eb0
Loris Accordino <119436498+LorisAccordino@users.noreply.github.com>
2026-04-20 11:22:41 +0200

    Remove form-validator.js redundant logic

--- public/assets/js/form-validator.js
+++ public/assets/js/form-validator.js
@@ -91,7 +91,7 @@ const FormValidator = {
    */
   isRequired: function (fieldId) {
     const field = document.getElementById(fieldId)
-    return field && (field.hasAttribute('required') || field.hasAttribute('richiesto'))
+    return field && (field.hasAttribute('required')
   },
 
   /**
